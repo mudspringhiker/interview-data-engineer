c5c1866ea311399223f2703b7132f090879e992e
Alona Varshal <thirdwave72@gmail.com>
2017-09-30 17:54:20 -0500

    Initial commit

---
+++ csv_to_json.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+
+def main():
+    csvfile = 'data.csv'
+    outputfile = 'output.json'
+    get_json_from_csv(csvfile, outputfile)
+    print("Conversion complete.")
+
+
+def get_json_from_csv(csvfile, outputfile):
+    """Converts a csv file to a json formatted data."""
+    with open(csvfile) as cf:
+        reader = csv.reader(cf)
+        csvRows = list(reader)
+    with open(outputfile, 'w') as jsonfile:
+        for row in csvRows[1:]:
+            line = {
+                "title": "Floor Access Event",
+                "type": "object",
+                "properties": {
+                    "person_id": row[0],
+                    "datetime": row[1].replace(' ', '-'),
+                    "floor_level": int(row[2]),
+                    "building": row[3]
+                },
+                "required": [row[0], row[1].replace(' ', '-'), int(row[2]), row[3]]
+            }
+            json.dump(line, jsonfile)
+            jsonfile.write('\n')
+
+
+if __name__ == '__main__':
+    main()
