466d8035921072c355397a5372e14d2615802a7b
Alona Varshal <thirdwave72@gmail.com>
2017-09-30 23:41:49 -0500

    Use csv.DictReader instead

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,34 +1,28 @@
 import csv
 import json
+from schema import schema
 
 
 def main():
     csvfile = 'data.csv'
     outputfile = 'output.json'
-    get_json_from_csv(csvfile, outputfile)
+    convert_to_json(csvfile, outputfile)
     print("Conversion complete.")
 
 
-def get_json_from_csv(csvfile, outputfile):
-    """Converts a csv file to a json formatted data."""
-    with open(csvfile) as cf:
-        reader = csv.reader(cf)
-        csvRows = list(reader)
-    with open(outputfile, 'w') as jsonfile:
-        for row in csvRows[1:]:
-            line = {
-                "title": "Floor Access Event",
-                "type": "object",
-                "properties": {
-                    "person_id": row[0],
-                    "datetime": row[1].replace(' ', '-'),
-                    "floor_level": int(row[2]),
-                    "building": row[3]
-                },
-                "required": ["person_id", "datetime", "floor_level", "building"]
-            }
-            json.dump(line, jsonfile)
-            jsonfile.write('\n')
+def convert_to_json(csvfile, outputjson, schema=schema):
+    with open(csvfile) as f:
+        reader = csv.DictReader(f)
+        
+        with open(outputjson, 'w') as jsonfile:
+            for row in reader:
+                schema['properties'] = {
+                    'building': row['Building'],
+                    'datetime': row['Floor Access DateTime'].replace(' ', '-'),
+                    'floor_level': int(row['Floor Level']),
+                    'person_id': row['Person Id']}
+                json.dump(schema, jsonfile)
+                jsonfile.write('\n')
 
 
 if __name__ == '__main__':
