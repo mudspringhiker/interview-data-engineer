c1a691dcfaaa9254494ebbe94bf3ffddd6271d47
Alona Varshal <thirdwave72@gmail.com>
2017-10-01 17:47:53 -0500

    Add conditionals, change how to format datetime

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,31 +1,48 @@
 import csv
 import json
+import datetime
 
 
 def main():
     csvfile = 'data.csv'
     outputfile = 'output.json'
-    convert_to_json(csvfile, outputfile)
+    save_json(convert_to_json(csvfile), outputfile)
     print("Conversion complete.")
 
 
-def convert_to_json(csvfile, outputjson):
+def convert_to_json(csvfile):
     with open(csvfile) as f:
         reader = csv.DictReader(f)
-        
-        with open(outputjson, 'w') as jsonfile:
-            for row in reader:
-                line = {}
+        json_list = []
+        for row in reader:
+            line = {}
+            if row['Building']:
                 line['building'] = row['Building']
-                line['datetime'] = row['Floor Access DateTime'].replace(' ', '-')
-                try:
-                    line['floor_level'] = int(row['Floor Level'])
-                except ValueError:
-                    line['floor_level'] = row['Floor Level']
+            else:
+                line['building'] = '99999'
+            try:
+                line['datetime'] = str((datetime.datetime.strptime(row['Floor Access DateTime'], \
+                         "%m/%d/%y %H:%M")).strftime("%m/%d/%y-%H:%M"))
+            except ValueError:
+                line['datetime'] = '99999'
+            try:
+                line['floor_level'] = int(row['Floor Level'])
+            except ValueError:
+                line['floor_level'] = 0
+
+            if row['Person Id']: 
                 line['person_id'] = row['Person Id']
-                
-                json.dump(line, jsonfile)
-                jsonfile.write('\n')
+            else:
+                line['person_id'] = '99999'
+            json_list.append(line)
+    return json_list
+
+
+def save_json(json_list, outputfile):
+    with open(outputfile, 'w') as jsonfile:
+        for line in json_list:
+            json.dump(line, jsonfile)
+            jsonfile.write('\n')
 
 
 if __name__ == '__main__':
